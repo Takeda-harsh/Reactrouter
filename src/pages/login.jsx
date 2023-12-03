4c0d7b9f7923162764f84bb61db6dabdc4f539dc
Kofi Akyea <kakyea13@gmail.com>
2023-12-03 21:25:49 +0000

    added login and sign up

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,30 +1,33 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 import '../App.css'
 
 export default function Login () {
     return (
         <>
            <div className="Login">
+            <div><div className="back-btn"><Link to= '/Home'>Back</Link> </div></div>
             <div className="login-details">
-                <div>
+                
+                <div className="login-header">
                     <h1>Login</h1>
                     <p>Get started with Shortly</p>
                 </div>
-                <div>
+                <div className="user-input">
                     <label htmlFor="email" >Email</label> <br/>
                     <input type="email"/>
                 </div>
                 
-                <div>
+                <div className="user-input">
                     <label htmlFor="password" >Password</label> <br/>
                     <input type="password"/>
                 </div>
 
                 <p className="error-message"></p>
 
-                <button>Login</button>
+                <button className="login-btn">Login</button>
 
-                <div>
+                <div className="account">
                     <p>Don't have an account yet? <span>Sign Up</span></p>
                 </div>
             </div>
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,10 +1,53 @@
 import React from "react";
+import {Link} from 'react-router-dom'
+import '../App.css'
 
-
-export default function SingUp () {
+export default function SignUp () {
     return (
         <>
-           <div>Sing Up</div>
+           <div className="Login">
+            <div><div className="back-btn"><Link to='/Home'>Back</Link> </div></div>
+            <div className="login-details">
+                
+                <div className="login-header">
+                    <h1>Sign up</h1>
+                    <p>Get started with Shortly</p>
+                </div>
+                <div className="user-name">
+                    <div>
+                    <label htmlFor="First Name" >First Name</label> <br/>
+                    <input type="text"/>
+                    </div>
+                    
+                    <div>
+                    <label htmlFor="Last Name" >Last Name</label> <br/>
+                    <input type="text"/>
+                    </div>
+                    
+                </div>
+                
+                <div className="user-input">
+                    <label htmlFor="password" >Password</label> <br/>
+                    <input type="password"/>
+                </div>
+                <div className="user-input">
+                    <label htmlFor="password" >Confirm Password</label> <br/>
+                    <input type="password"/>
+                </div>
+
+                <p className="error-message"></p>
+
+                <button className="login-btn">Login</button>
+
+                <div className="account">
+                    <p>Don't have an account yet? <span>Sign Up</span></p>
+                </div>
+            </div>
+
+            <div className="login-image">
+
+            </div>
+           </div>
         </>
     )
 }
