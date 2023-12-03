fdfba405773725301849b82b50371993f5c480a2
Kofi Akyea <kakyea13@gmail.com>
2023-12-03 17:54:32 +0000

    added login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,38 @@
 import React from "react";
-
+import '../App.css'
 
 export default function Login () {
     return (
         <>
-           <div>Login</div>
+           <div className="Login">
+            <div className="login-details">
+                <div>
+                    <h1>Login</h1>
+                    <p>Get started with Shortly</p>
+                </div>
+                <div>
+                    <label htmlFor="email" >Email</label> <br/>
+                    <input type="email"/>
+                </div>
+                
+                <div>
+                    <label htmlFor="password" >Password</label> <br/>
+                    <input type="password"/>
+                </div>
+
+                <p className="error-message"></p>
+
+                <button>Login</button>
+
+                <div>
+                    <p>Don't have an account yet? <span>Sign Up</span></p>
+                </div>
+            </div>
+
+            <div className="login-image">
+
+            </div>
+           </div>
         </>
     )
 }
